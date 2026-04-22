8c6d274a1dbd929c9b80d5ab05cc2324c6f9dc73
anaumov <roflsham@mail.ru>
2026-04-22 18:49:05 +0300

    add main service, repository classes

--- src/main/java/ru/nau/spring_ai_workshop/chat/ChatController.java
+++ src/main/java/ru/nau/spring_ai_workshop/chat/ChatController.java
@@ -1,21 +1,49 @@
 package ru.nau.spring_ai_workshop.chat;
 
-import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import ru.nau.spring_ai_workshop.model.Chat;
+import ru.nau.spring_ai_workshop.service.ChatService;
 
-@RestController
+
+@Controller
 public class ChatController {
+    private final ChatService chatService;
 
-    private final OllamaChatModel ollamaChatModel;
-
-    public ChatController(OllamaChatModel ollamaChatModel) {
-        this.ollamaChatModel = ollamaChatModel;
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
     }
 
-    @GetMapping("/chat")
-    public String chat(@RequestParam(defaultValue = "Придумай короткую шутку о котах") String prompt) {
-        return ollamaChatModel.call(prompt);
+    @GetMapping("/")
+    public String mainPage(ModelMap model) {
+        model.addAttribute("chats", chatService.getAllChats());
+        return "chat";
+    }
+
+    @GetMapping("/chat/{chatId}")
+    public String showChat(ModelMap model, @PathVariable Long chatId) {
+        model.addAttribute("chats", chatService.getAllChats());
+        model.addAttribute("chat", chatService.getChat(chatId));
+        return "chat";
+    }
+
+    @PostMapping("/chat/new")
+    public String newChat(@RequestParam String title) {
+        Chat chat = chatService.createChat(title);
+        return "redirect:/chat/" + chat.getId();
+    }
+
+    @PostMapping("/chat/{chatId}/delete")
+    public String deleteChat(@PathVariable Long chatId) {
+        chatService.deleteChat(chatId);
+        return "redirect:/";
+    }
+
+    @PostMapping("/chat/{chatId}/entry")
+    public String talkToModel(@PathVariable Long chatId, @RequestParam String prompt) {
+        chatService.proceedInteraction(chatId, prompt);
+        return "redirect:/chat/" + chatId;
     }
 }
+
---
+++ src/main/java/ru/nau/spring_ai_workshop/model/Role.java
@@ -0,0 +1,21 @@
+package ru.nau.spring_ai_workshop.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+@Getter
+public enum Role {
+
+    USER("user"), ASSISTANT("assistant"), SYSTEM("system");
+
+    private final String role;
+
+    public static Role getRole(String roleName) {
+        return Arrays.stream(Role.values()).filter(role -> role.role.equals(roleName)).findFirst().orElseThrow();
+    }
+
+}
+
